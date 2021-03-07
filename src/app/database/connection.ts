02f211533a8e20b8279166d729c96167b713ae6b
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-03-07 15:53:45 -0300

    change database connection
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/app/database/connection.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const mongoPort = process.env.MONGO_PORT || '27017';
+const mongoUser = process.env.MONGO_INITDB_ROOT_USERNAME || 'root';
+const mongoPass = process.env.MONGO_INITDB_ROOT_PASSWORD || 'root';
+const mongoDb = process.env.MONGO_DATABASE || 'test';
+
+const mongoUrl = `mongodb://${mongoUser}:${mongoPass}@${mongoHost}:${mongoPort}`;
+
+const connect = () => {
+  mongoose
+    .connect(`${mongoUrl}/${mongoDb}?authSource=admin`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((_) => {
+      console.log('Database connected');
+    })
+    .catch((error) => {
+      console.log('Error: ', error);
+    });
+};
+
+connect();
